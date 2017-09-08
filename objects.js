6a04232b447b20f6d34cc7e422c9a0a38c984852
gitluky <jluk87@gmail.com>
2017-09-08 23:11:31 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {ZAYN: 'PILLOWTALK', John Mayer: 'Free Fallin', Adele: 'Hiding My Heart', Brandi Carlile: 'If There Was No You'}
+
+function updatePlaylist(obj, artistName, songTitle){
+  updatedPlaylist = Object.assign({}, obj, {artistName: songTitle})
+  return updatedPlaylist
+}
+
+function removeFromPlaylist(obj, artistName){
+  updatedPlaylist = Object.assign({}, obj)
+  delete updatedPlaylist.artistName
+  return updatedPlaylist
+}
